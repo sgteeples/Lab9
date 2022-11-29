19323f3de2eaaf64304e4d281eeaa96e25e0d726
sgteeples <104848918+sgteeples@users.noreply.github.com>
2022-11-29 10:27:04 -0700

    Add files via upload

---
+++ projectiles.h
@@ -0,0 +1,76 @@
+#ifndef PROJECTILES
+#define PROJECTILES
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* The same projectile structure will be used for all projectiles in the game,
+so this enum is used to identify the type of projectile */
+typedef enum {
+  PROJECTILE_TYPE_EGG,
+  PROJECTILE_TYPE_GUN
+} projectile_type_t;
+
+/* This struct contains all information about a projectile */
+typedef struct {
+
+  // Missle type (player, enemy, enemy plane)
+  projectile_type_t type;
+
+  // Current state (the 'enum' will be defined in your projectile.c file, so it's
+  // just declared as an integer type here)
+  int32_t currentState;
+
+  // Starting x,y of projectile
+  uint16_t x_origin;
+  uint16_t y_origin;
+
+  // Ending x,y of projectile, and the total length from origin to destination.
+  uint16_t x_dest;
+  uint16_t y_dest;
+  uint16_t total_length;
+
+  // Used to track the current x,y of projectile
+  int16_t x_current;
+  int16_t y_current;
+
+  // While flying, this tracks the current length of the flight path
+  double length;
+
+  // While flying, this flag is used to indicate the projectile should be detonated
+  bool die_me;
+
+  // Used for game statistics, this tracks whether the projectile impacted the
+  // ground.
+  bool egged;
+
+} projectile_t;
+
+////////// State Machine INIT Functions //////////
+
+// Initialize eggs and projectiles as a dead .  This is useful at the start of the
+// game to ensure that player and plane projectiles aren't moving before they
+// should.
+void projectile_init_dead(projectile_t *projectile);
+
+// Initialize the projectile as a player projectile.  This function takes an (x, y)
+// destination of the projectile (where the user touched on the touchscreen).  The
+// origin should be the closest "firing location" to the destination (there are
+// three firing locations evenly spaced along the bottom of the screen).
+void projectile_init_gun(projectile_t *projectile, uint16_t x_dest, uint16_t y_dest);
+
+// Initialize the projectile as a plane projectile.  This function takes an (x, y)
+// location of the plane which will be used as the origin.  The destination can
+// be randomly chosed along the bottom of the screen.
+void projectile_init_egg(projectile_t *projectile, int16_t plane_x, int16_t plane_y);
+
+////////// State Machine TICK Function //////////
+void projectile_tick(projectile_t *projectile);
+
+// Return whether the given projectile is dead.
+bool projectile_is_dead(projectile_t *projectile);
+
+// Return whether the given projectile is flying.
+bool projectile_is_flying(projectile_t *projectile);
+
+#endif /* PROJECTILES */
